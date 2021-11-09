0bfe83a255230153e3d0ec7a4b193d5ac0f47a66
Luka Marčič <marcicluke@gmail.com>
2021-11-10 00:12:42 +0100

    neuspel poiskus popravljanja

--- Zajem_podatkov.py
+++ Zajem_podatkov.py
@@ -4,26 +4,30 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+
 stran = requests.get('https://audioboom.com/channels/4954758.rss')
 
 soup = BeautifulSoup(stran.text, 'html.parser')
 
-ime_datoteke = "DTFH.csv"
-datoteka = csv.writer(open(ime_datoteke, 'w', newline='', encoding='utf-8'))
-datoteka.writerow(['Naslov', 'Spletna povezava', 'Dolžina', 'Datum', 'Eksplicitnost', 'Opis'])
-
+#Beautiful soup nam naredi seznam epizod
 epizode = soup.select("item")
-stevec=0
-print(stevec)
+
+podatki_epizod = []
+
+#za vsako epizodo izluščimo podatke (podatki so pretvorjeni v string, odstranjeni so začetki in konci, tam so bile html oznake)
+#edina izjema je spletna povezava, saj beautiful soup zaradi nekega razloga spremeni format in ga potem ne najde
 for epizoda in epizode:
-    stevec += 1
     naslov = (str(epizoda.find("title")))[7:-8]
     spletna_povezava = (str((re.findall(r'link/>.*', str(epizoda)))[0]))[6:]
     dolzina = (str(epizoda.find("itunes:duration")))[17: -18]
     datum = (str(epizoda.select_one("pubDate")))[9:-10]
     eksplicitnost = (str(epizoda.find("itunes:explicit")))[17:-18]
     opis = ((str(epizoda.find("description")))[13:-14]).replace("\r", " ").replace("\n", " ")
-    datoteka.writerow([naslov, spletna_povezava, dolzina, datum, eksplicitnost, opis])
+    podatki_epizod.append([naslov,spletna_povezava, dolzina, datum, eksplicitnost, opis])
 
+#seznam epizod sedaj zapišemo v csv datoteko
 
-print(stevec)
+ime_datoteke = "DTFH.csv"
+datoteka = csv.writer(open(ime_datoteke, 'w', newline='', encoding='utf-8'))
+datoteka.writerow(['Naslov', 'Spletna povezava', 'Dolžina', 'Datum', 'Eksplicitnost', 'Opis'])
+datoteka.writerows(podatki_epizod)
